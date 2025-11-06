1f0544a3c08e186be72ec049d24ffebd8c16e0b9
XavZol <alansolis707@gmail.com>
2025-11-06 14:50:28 -0600

    Primer programa con funciones, entendiendo python.

---
+++ Primerprog.py
@@ -0,0 +1,8 @@
+
+def funcion(informacion, trabajo = "Desconocido"): 
+ print(f'hola {informacion} estoy {trabajo}')
+ 
+ funcion("alan", "programando")
+ funcion("javier", "diseñador")
+ funcion("pinguino", "doctor")
+ funcion("azul", "mecanico")
