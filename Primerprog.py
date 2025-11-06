99358e13598a462de3fa11541a45eb1480f73cfe
XavZol <alansolis707@gmail.com>
2025-11-06 15:04:52 -0600

    Números

--- Primerprog.py
+++ Primerprog.py
@@ -1,9 +1,22 @@
-nombre = 'Pedro'
+print(4)
+print(5.4)
 
-def mostrar_nombre(nombre):
-    print(f'Hola {nombre}')
+print(2 + 8)
+print(12 - 4)
+print(4 * 3)
+print(8 / 2)
 
-mostrar_nombre(nombre)
+print(2 ** 3)
 
-print(nombre.upper())
-print(nombre.title())
+print((2 + 8) * 7)
+
+numero = 40
+print(numero)
+numero += 1
+print(numero)
+numero += 4
+print(numero)
+
+
+
+
