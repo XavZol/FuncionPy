c8dd3ea6a33b05b41aba42ec97b1e2fa651211ac
XavZol <alansolis707@gmail.com>
2025-11-06 20:23:31 -0600

    Arrays con python

--- Primerprog.py
+++ Primerprog.py
@@ -1,11 +1,35 @@
-def suma(a = 0, b = 0):
-    print(a + b)
-
-suma( 2 , 3)
-suma( 8 , 4)
-suma( 4 , 7)
-
-def resta(a,b):
-    print(a - b)
-
-resta(5, 2)
+lenguajes = ['Python', 'kotlin', 'java', 'javas']
+    print(lenguajes)
+    
+    print(lenguajes[0])
+    #ordenar 
+    lenguajes.sort()
+    print(lenguajes)
+    
+    #Acceder a un elemento dentro de un texto
+    print = f'Estoy aprendiendo' {lenguajes[3]}
+    print(aprendiendo)
+    
+    #Modificar valores de un arreglo
+    lenguajes[2] = 'PHP'
+    print(lenguajes)
+    
+    #Agregar elementos a un arreglo
+    lenguajes.append('Ruby')
+    print(lenguajes)
+    
+    
+    #Eliminar elementos de un arreglo
+    del lenguajes[1]
+    print(lenguajes)
+    
+    #eliminar elementos de un arreglo
+    lenguajes.pop(0)
+    print(lenguajes)
+    
+    #eliminar los elementos
+    lenguajes.remove('PHP')
+    print(lenguajes)
+    
+    
+    
