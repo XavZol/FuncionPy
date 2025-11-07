a806f1048767e7143bd4732e5ee6d03b8047def4
XavZol <alansolis707@gmail.com>
2025-11-06 20:43:40 -0600

    Iteradores con Python

--- Primerprog.py
+++ Primerprog.py
@@ -1,35 +1,11 @@
-lenguajes = ['Python', 'kotlin', 'java', 'javas']
-    print(lenguajes)
+lenguajes = ['Python', 'kotlin', 'java', 'javas','PHP','Ruby', 'GO']
+
+#iterador 
+for lenguaje in lenguajes:
+    print(f'lenguaje{lenguaje}')
     
-    print(lenguajes[0])
-    #ordenar 
-    lenguajes.sort()
-    print(lenguajes)
+    print('Texto')
     
-    #Acceder a un elemento dentro de un texto
-    print = f'Estoy aprendiendo' {lenguajes[3]}
-    print(aprendiendo)
-    
-    #Modificar valores de un arreglo
-    lenguajes[2] = 'PHP'
-    print(lenguajes)
-    
-    #Agregar elementos a un arreglo
-    lenguajes.append('Ruby')
-    print(lenguajes)
-    
-    
-    #Eliminar elementos de un arreglo
-    del lenguajes[1]
-    print(lenguajes)
-    
-    #eliminar elementos de un arreglo
-    lenguajes.pop(0)
-    print(lenguajes)
-    
-    #eliminar los elementos
-    lenguajes.remove('PHP')
-    print(lenguajes)
-    
-    
-    
+#For que escriba numeros
+for numero in range(0, 10):
+    print(numero)
