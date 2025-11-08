75f86e3d324c869cf283bb51e98ae5c38de1509b
XavZol <alansolis707@gmail.com>
2025-11-08 05:41:27 -0600

    Tercer ejemplo con un en un for

--- Primerprog.py
+++ Primerprog.py
@@ -1,12 +1,7 @@
-#operadores and y or
+lenguajes = ['Python', 'Kotlin', 'Java', 'Javascript']
 
-usuario_logueado= True
-usuario_admin= True
-
-if usuario_logueado and usuario_admin:
-    print('Administrador')
-elif usuario_logueado:
-    print('Acceso al sistema')
-else:
-    print('Debes iniciar sesion')
-    
+for lenguaje in lenguajes:
+    if lenguaje == 'Python':
+        print(lenguaje.upper() )
+    else:
+        print(lenguaje)
